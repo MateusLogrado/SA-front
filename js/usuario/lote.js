2af4d7a7951ec99bc4d4b60a19a410c87748354f
Mateus Logrado <mateus_logrado@estudante.sesisenai.org.br>
2025-07-15 14:19:06 -0300

    cadastrar lote completo

---
+++ js/usuario/lote.js
@@ -0,0 +1,39 @@
+let resLote = document.getElementById("res")
+let lote = document.getElementById("lote")
+
+lote.addEventListener("click", (e)=>{
+    e.preventDefault()
+
+    fetch("https://dummyjson.com/users",{
+        method: "GET",
+        headers: { "content-type":"application/json" }
+    })
+    .then(resp => resp.json())
+    .then(dados=> {
+        dados.users.forEach(dad => {
+            const valores = {
+                firstName: dad.firtsName,
+                lastName: dad.lastName,
+                age: dad.age,
+                email: dad.email,
+                phone: dad.phone,
+                city: dad.city,
+                state: dad.state,
+                birthDate: dad.birthDate
+            }
+
+            fetch(`http://localhost:3000/usuario`, {
+                method: "POST",
+                headers: { "content-type":"application/json" },
+                body: JSON.stringify(valores)
+            })
+            .then(resp => resp.body)
+            .then(()=>{
+                resLote.innerHTML = "lote registrado com sucesso"
+            })
+        })
+    })
+    .catch((err)=>{
+        console.error("erro: ", err)
+    })
+})
